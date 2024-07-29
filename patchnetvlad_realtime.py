0eef0b3e8bcef89316b693c5e74dbbbfc78a3744
cvlab moon <ans92175@gmail.com>
2024-07-29 05:02:53 +0000

    add webcam code(for test)

--- patchnetvlad_realtime.py
+++ patchnetvlad_realtime.py
@@ -1,5 +1,26 @@
+import torch
+import torch.nn as nn
+import cv2
+
+from patchnetvlad.models.models_generic import get_backend, get_model
+from patchnetvlad.tools import PATCHNETVLAD_ROOT_DIR
+from match_two import match_two
+
 def main():
-    pass
+    vid = cv2.VideoCapture(0)
+    
+    if vid.isOpened():
+        while True:
+            ret, img = vid.read()
+            if ret:
+                cv2.imshow('camera', img)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+
+    vid.release()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
