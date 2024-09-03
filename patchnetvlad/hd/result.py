d3983d17d03004f33a86e3d87e605fa8e99515de
moon <ans92175@gmail.com>
2024-09-03 15:54:13 +0900

    reault init

---
+++ patchnetvlad/hd/result.py
@@ -0,0 +1,13 @@
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
